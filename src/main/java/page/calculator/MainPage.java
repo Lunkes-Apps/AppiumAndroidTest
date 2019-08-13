e9902a5aa51ace33465e963bef8ed867aa34ecab
Alexandre de Almeida Lunkes <alexandre.lunkes@kaiostech.com>
2019-08-13 11:23:54 -0400

    Creating steps definitions

---
+++ src/main/java/page/calculator/MainPage.java
@@ -0,0 +1,28 @@
+package page.calculator;
+
+import core.BasePage;
+
+public class MainPage extends BasePage{
+	
+	public MainPage() {
+		super("com.google.android.calculator", "com.android.calculator2.Calculator");		
+	}
+	
+	private static final NUMBER_0_LOCATOR;
+	private static final NUMBER_1_LOCATOR;
+	private static final NUMBER_2_LOCATOR;
+	private static final NUMBER_3_LOCATOR;
+	private static final NUMBER_4_LOCATOR;
+	private static final NUMBER_5_LOCATOR;
+	private static final NUMBER_6_LOCATOR;
+	private static final NUMBER_7_LOCATOR;
+	private static final NUMBER_8_LOCATOR;
+	private static final NUMBER_9_LOCATOR;
+	
+	private static final KEY_EQUAL_LOCATOR;
+	
+	public void pressNumber(String number) {
+		
+	}
+	
+}
--- src/main/java/test/stepDefinitions/calculator/StepsCalculatorTest.java
+++ src/main/java/test/stepDefinitions/calculator/StepsCalculatorTest.java
@@ -36,4 +36,6 @@ public class StepsCalculatorTest {
 	public void tearDown() {		
 	
 	}
+	
+	
 }
